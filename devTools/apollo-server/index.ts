845f965ba8a6e47bbc6347aac7e70a244c692330
tetymd <yamada.tetsuya258@gmail.com>
2021-06-26 15:51:55 +0900

    CRUD対応のメソッドを追加＆スキーマを別ファイルに分割

--- devTools/apollo-server/index.ts
+++ devTools/apollo-server/index.ts
@@ -1,32 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { ApolloServer } from 'apollo-server'
+const { readFileSync } = require('fs')
+
 const prisma = new PrismaClient()
 
 async function main() {
-  const typeDefs = `
-    type User {
-      id: String
-      userId: String
-      name: String
-      email: String!
-      createdAt: String
-      role: String
-      posts: [Post!]
-    }
-    type Post {
-      id: String
-      title: String
-      content: String
-      createdAt: String
-      authorId: String
-      updateAt: String
-      published: Boolean
-    }
-    type Query {
-      allUsers: [User!]!
-      allPosts: [Post!]
-    }
-  `
+  const typeDefs = readFileSync('./apollo-server/schema.gql').toString('utf-8')
   const resolvers = {
     Query: {
       allUsers: () => {
@@ -37,6 +16,49 @@ async function main() {
       allPosts: () => {
         return prisma.post.findMany()
       },
+      getUserById: (_: any, args: { id: number }) => {
+        const id = +args.id
+        return prisma.user.findUnique({
+          where: { id }
+        })
+      }
+    },
+    Mutation: {
+      createPost: (_: any, args: {
+        title: string,
+        content: string,
+        authorId: number
+      }) => {
+        const id = +args.authorId
+        return prisma.post.create({
+          data: {
+            title: args.title,
+            content: args.content,
+            authorId: id
+          }
+        })
+      },
+      updatePost: (_: any, args: {
+        title: string,
+        content: string,
+        postId: number
+      }) => {
+        const id = +args.postId
+        return prisma.post.update({
+          where: { id: id },
+          data: {
+            title: args.title,
+            content: args.content
+          }
+        })
+      },
+      deletePost: (_: any, args: {
+        postId: number
+      }) => {
+        const id = +args.postId
+        prisma.post.delete({ where: { id: id } })
+        return `Delete post ID: ${args.postId}`
+      }
     }
   }
   const server = new ApolloServer({ resolvers, typeDefs })
