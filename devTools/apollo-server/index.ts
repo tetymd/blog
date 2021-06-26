1f0ef33f15d0f75ab47ed54d46436ca49b67e5e2
tetymd <yamada.tetsuya258@gmail.com>
2021-06-26 13:33:12 +0900

    スキーマ修正

--- devTools/apollo-server/index.ts
+++ devTools/apollo-server/index.ts
@@ -5,18 +5,38 @@ const prisma = new PrismaClient()
 async function main() {
   const typeDefs = `
     type User {
-      email: String!
+      id: String
+      userId: String
       name: String
+      email: String!
+      createdAt: String
+      role: String
+      posts: [Post!]
+    }
+    type Post {
+      id: String
+      title: String
+      content: String
+      createdAt: String
+      authorId: String
+      updateAt: String
+      published: Boolean
     }
     type Query {
       allUsers: [User!]!
+      allPosts: [Post!]
     }
   `
   const resolvers = {
     Query: {
       allUsers: () => {
-        return prisma.user.findMany()
-      }
+        return prisma.user.findMany({
+          include: { posts: true },
+        })
+      },
+      allPosts: () => {
+        return prisma.post.findMany()
+      },
     }
   }
   const server = new ApolloServer({ resolvers, typeDefs })
