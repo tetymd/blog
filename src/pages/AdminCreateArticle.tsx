8ef0e566dee5ccd96a2708a14e7a2ac101d206c0
tetymd <yamada.tetsuya258@gmail.com>
2021-07-13 00:35:52 +0900

    重複したコンポーネントを削除

--- src/pages/AdminCreateArticle.tsx
+++ src/pages/AdminCreateArticle.tsx
@@ -62,7 +62,6 @@ export default function AdminCreateArticle() {
         <Mutation mutation={CREATE_POST} update={updateMutation}>
           { PipeEditor }
         </Mutation>
-        <AdminArticleEditorCreate />
       </CtmBox>
     </Box>
   )
